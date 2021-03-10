d52fd9f8b0ed79433f05030b208bf131faae6d1a
ABURGER219014868 <219014868@mycput.ac.za>
2021-03-10 14:25:45 +0200

    Build better addition()

--- src/za/ac/cput/calculator/Addition.java
+++ src/za/ac/cput/calculator/Addition.java
@@ -1,4 +1,24 @@
 package za.ac.cput.calculator;
 
 public class Addition {
+    /**
+     * This adds two integers
+     * @param a = a
+     * @param b = b
+     * @return a+b
+     */
+    public static int integerSum(int a, int b){
+        return a + b;
+    }
+
+    /**
+     * This adds two real numbers
+     * @param a
+     * @param b
+     * @return
+     */
+    public static double doubleSum(double a, double b){
+        return a + b;
+    }
+
 }
