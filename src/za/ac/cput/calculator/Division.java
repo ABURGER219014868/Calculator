32f4e3b3b86b0940c19c0ca998e5bb721f603465
ABURGER219014868 <219014868@mycput.ac.za>
2021-03-10 14:49:33 +0200

    Add Integer Division() comment

--- src/za/ac/cput/calculator/Division.java
+++ src/za/ac/cput/calculator/Division.java
@@ -2,6 +2,12 @@ package za.ac.cput.calculator;
 
 public class Division {
 
+    /**
+     * Returns integer division result
+     * @param numerator
+     * @param denominator
+     * @return
+     */
     public static int integerDivision(int numerator, int denominator){
         if(denominator < 1) return 0;
         return numerator/denominator;
